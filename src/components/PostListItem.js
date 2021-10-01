2ad31c2b9d8965557a08683683797b21827a0dfe
Jorge196 <JoRamirez96@gmail.com>
2021-10-01 18:12:03 -0400

    added the like button to posts

--- src/components/PostListItem.js
+++ src/components/PostListItem.js
@@ -1,10 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, {useState}from 'react';
+import { Link } from 'react-router-dom';
 
 const PostListItem = ({ post }) => {
+    const [count, setCount] = useState(0);
+
+
+
     return (
             <li className="" key={post.id}>
                 <Link to={`/posts/${post.id}`}>{post.name}</Link>
+                <span>{" "}</span> 
+                    <button className="bg-purple-700 text-yellow-200 rounded-lg border-2 border-yellow-400 hover:bg-indigo-700 hover:border-red-600 my-2"onClick={() => setCount(count + 1)} id={post.id}> Like: {count} </button>
+                 
             </li> 
     );
 } 
--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -1,19 +1,27 @@
 import React from 'react'
 import PostListItem from './PostListItem';
 
-const PostsList = ({posts}) => {
-    return (
-        <>
-            <h1 className="text-center text-2xl font-semibold mb-2 text-purple-500">Posts List</h1>
+class PostsList extends React.Component  {
+    
+    
+    
+    render(){
+       return (
             <div className="">
-                <figure className="grid grid-cols-3 gap-3 p-4 shadow bg-yellow-300 rounded-lg">
-                    <ul className="text-blue-400 hover:text-purple-600 ">
-                        {posts.map(post => <PostListItem key={post.id} post={post}/>)}
-                    </ul>
-                </figure>   
+                <h1 className="text-center text-2xl font-semibold mb-2 text-purple-500">Posts List</h1>
+                
+                    <figure className="grid grid-cols-3 gap-3 p-4 shadow bg-yellow-300 rounded-lg">
+                        <ul className="text-blue-400 hover:text-purple-600 ">
+                            {this.props.posts.map(post => 
+                            <PostListItem key={post.id} post={post}/> 
+                            )}
+                        </ul>
+                    </figure>   
             </div>
-        </>
-    );
-};
+       )
+ 
+    }
+    
+}
 
 export default PostsList;
